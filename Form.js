fd2d4817bc54ce9af741aba6c3c5dcd6b166bc3c
Tharani <127316853+Tharanisekar@users.noreply.github.com>
2024-07-14 14:35:16 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,27 @@
+import Button from './Button'
+import React from 'react';
+
+const Form =({reqType , setReqType}) =>{
+    return(
+        <form onSubmit={(e)=>e.preventDefault()}>
+        <Button
+        buttonText ="users"
+        reqType={reqType}
+        setReqType={setReqType}
+        />
+        <Button
+        buttonText ="posts"
+        reqType={reqType}
+        setReqType={setReqType}
+        />
+        <Button
+        buttonText ="comments"
+        reqType={reqType}
+        setReqType={setReqType}
+        />
+
+        </form>
+    )
+}
+
+export default Form
